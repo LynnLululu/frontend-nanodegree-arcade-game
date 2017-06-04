8c97bf0f7265bff825af7e1a46ee14dd47af2f69
LynnLululu <luyiqun130@foxmail.com>
2017-06-04 18:15:59 +0800

    Basic Function
    Added 6 enemies, 1 player.
    Made enemies move across the screen over and over again.
    Set key control of the player, and prevent it going off the canvas.
    Set the player to the starting point when it reaches the river.
    Set the player to the starting point when it collide with the enemies.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,s) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;    // x position
+    this.y = y;    // y position
+    this.s = s;    // speed charactor used in .update method
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.s * dt;  // set the movement and speed of the enemy 
+
+    if (this.x > 505) {    // if the enemy went out of the screen,
+        this.x = -101;     // set them back to the starting point.
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +32,71 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 202;
+    this.y = 404;
+    this.sprite = 'images/char-boy.png'
+};
 
+Player.prototype.update = function() {
+    if (this.y < 0) {   // When the Player reaches the watherside.
+        this.x = 202;   // reset it to the begin point.
+        this.y = 404;
+    } else if (this.y == 238) {
+        var dis1 = Math.abs(this.x - allEnemies[2].x);
+        var dis2 = Math.abs(this.x - allEnemies[5].x);
+    } else if (this.y == 155) {
+        var dis1 = Math.abs(this.x - allEnemies[1].x);
+        var dis2 = Math.abs(this.x - allEnemies[4].x);
+    } else if (this.y == 72) {
+        var dis1 = Math.abs(this.x - allEnemies[0].x);
+        var dis2 = Math.abs(this.x - allEnemies[3].x);
+    }
+    if (dis1 < 80 || dis2 < 80) {
+        this.x = 202;
+        this.y = 404;
+    }
 
+};
+
+Player.prototype.checkCollisions = function() {
+    
+};
+
+Player.prototype.render = function(dt) {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// This handleInput() method use the keyCode from allowedKeys
+// to move the Player.
+// And it prevent the Player go off the canvas.
+Player.prototype.handleInput = function (move){
+    if (move == 'left' && this.x > 100) {
+        this.x -= 101;
+    } else if (move == 'right' && this.x < 350) {
+        this.x += 101;
+    }  else if (move == 'up' && this.y > 0) {
+        this.y -= 83;
+    }  else if (move == 'down' && this.y < 350) {
+        this.y += 83;
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player;
+
+var enemy1 = new Enemy(-101,60,280);
+var enemy2 = new Enemy(-101,145,500);
+var enemy3 = new Enemy(-101,225,470);
+var enemy4 = new Enemy(-202,60,230);
+var enemy5 = new Enemy(-202,145,400);
+var enemy6 = new Enemy(-202,225,350);
+
+var allEnemies = [enemy1,enemy2,enemy3,enemy4,enemy5,enemy6];
+
+
+
 
 
 
@@ -44,3 +112,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
